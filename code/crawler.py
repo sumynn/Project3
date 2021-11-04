58238e4d4e066bfd44aef90a66308c37db1b19a0
sumynn <sumynn.kim@gmail.com>
2021-11-04 17:54:33 +0900

    Update crawler - 버튼 대신 href 사용

--- code/crawler.py
+++ code/crawler.py
@@ -1,49 +1,59 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 import time
 import pandas as pd
+import math
 
 # 웹 드라이버 실행
 driver = webdriver.Chrome('./chromedriver_win32/chromedriver.exe')
+driver2 = webdriver.Chrome('./chromedriver_win32/chromedriver.exe')
+driver.implicitly_wait(time_to_wait=5)
+driver2.implicitly_wait(time_to_wait=5)
 
 url = "https://www.tripadvisor.com/Attractions-g187070-Activities-a_allAttractions.true-France.html"
 driver.get(url)
 
-driver.implicitly_wait(time_to_wait=5)
-
 France = pd.DataFrame()
 
-for i in range(29):
-    driver.find_elements_by_css_selector('header > div > div > a:nth-child(1)')[i].click()
-    time.sleep(5)
-    driver.switch_to_window(driver.window_handles[1])
+attractions = driver.find_elements_by_css_selector('header > div > div > a:nth-child(1)')
 
-    attraction_name = driver.find_elements_by_css_selector('h1')[1].text  # 관광지명
-    rating = driver.find_elements_by_css_selector('div.WlYyy.cPsXC.fksET.cMKSg')[0].text  # 전체 평점
+for attraction in attractions:
+    print("Count:", len(France))
+    attraction_url = attraction.get_attribute('href')
+    driver2.get(attraction_url)
+    time.sleep(3)
+
+    attraction_name = driver2.find_elements_by_css_selector('h1')[1].text  # 관광지명
+    rating = driver2.find_elements_by_css_selector('div.WlYyy.cPsXC.fksET.cMKSg')[0].text  # 전체 평점
 
     # DataFrame 저장
     attraction = {'attraction':attraction_name, 'score':rating}
     France = France.append(attraction, ignore_index=True)  
-
-    for i in range(100):
-        try:              
-            # 리뷰 가져오기
-            reviews = driver.find_elements_by_css_selector('div.bPhtn > div')
-            for review in reviews[:-1]:
+        
+    review_count = int(driver2.find_element_by_css_selector('div.cIUfa.Ci').text.split()[-1].replace(',', ''))
+    
+    # 리뷰 수에 따른 페이지 범위 설정
+    for i in range(math.floor(review_count / 10)):
+        # 리뷰 가져오기
+        reviews = driver2.find_elements_by_css_selector('div.bPhtn > div')
+        for review in reviews[:-1]:
+            try:   
                 rating = review.find_element_by_css_selector('div:nth-child(3) > svg').get_attribute('title').split()[0]  # 개인 평점
                 title = review.find_element_by_css_selector('div.WlYyy.cPsXC.bLFSo.cspKb.dTqpp').text  # 리뷰 제목
-                content = review.find_elements_by_css_selector('div.duhwe._T.bOlcm')[0].text  # 리뷰 내용
-                trip_date = review.find_element_by_class_name('fEDvV').text.split('•')[0].strip()  # 여행 날짜
+                content = review.find_element_by_css_selector('div.duhwe._T.bOlcm').text  # 리뷰 내용
+                trip_date = review.find_element_by_class_name('fEDvV').text.split(' • ')[0]  # 여행 날짜
 
                 # DataFrame 저장
                 attraction = {'attraction':attraction_name, 'score':rating, 'title':title, 'review':content, 'trip_date':trip_date}
                 France = France.append(attraction, ignore_index=True)
+            except Exception as ex:
+                print('ERROR!', ex)
+                pass
+        
+        # 최대 1000개까지만 가져옴
+        if i == 99:
+            break
 
-            # 다음 페이지로 이동
-            driver.find_element_by_xpath('//*[@id="tab-data-qa-reviews-0"]/div/div[5]/div[11]/div[1]/div/div[1]/div[2]/div/a').send_keys(Keys.ENTER)
-            time.sleep(5)
-        except:
-            continue
-
-    driver.close()
-    driver.switch_to_window(driver.window_handles[0])
+        # 다음 페이지로 이동
+        next_page = driver2.find_element_by_css_selector('div.cCnaz > div > a').get_attribute('href')
+        driver2.get(next_page)
+        time.sleep(3)
